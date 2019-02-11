51f09d080ace91cfdd57a388ddadf66bafbb5e6e
tomtrein <thomas.somers@zwijsencollege.org>
2019-02-11 20:58:52 +0100

    New Files for Push
    VERSION 1:
    
    - Installing Moonsharp basics
    - - 1 Hour
    - Setting up PlayerScript
    - - 2 Hours
    - Creating simple editor/scene
    - - 3.5 Hours
    - Implementing Proxies
    - - 4 Hours
    
    VERSION 2 (current):
    
    Setup Scene (Sprite)
    - 2 Hours
    
    Setup basic Robot
    - 1 Hour
    
    Setup Movement
    - 1 Hour
    
    Selection of tiles
    - 1 Hour
    
    Robot Classes
    - 1 Hour

---
+++ Assets/Pieecs/Scripts/Selection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selection : MonoBehaviour
+{
+
+	public SpriteRenderer renderer;
+
+	public bool enabled { get; private set; }
+
+
+	public void setEnabled(bool en)
+	{
+		enabled = en;
+		renderer.enabled = en;
+	}
+	
+	public bool getEnabled()
+	{
+		return enabled;
+	}
+
+	public void setColor(Color col)
+	{
+		renderer.color = col;
+	}
+
+	public Color getColor()
+	{
+		return renderer.color;
+	}
+	
+	
+	
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		renderer = this.GetComponent<SpriteRenderer>();
+		
+		setEnabled(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
